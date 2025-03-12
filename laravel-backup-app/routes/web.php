58fa41429fb0063a5abc09d8c161eba1298c01ca
Roza Mesfin <rozitamine70@gmail.com>
2025-03-12 19:05:23 +0300

    web.php the route updated

--- laravel-backup-app/routes/web.php
+++ laravel-backup-app/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\DatabaseBackupController;
+
+Route::get('database-backup', [DatabaseBackupController::class, 'backup'])->name('database.backup');
